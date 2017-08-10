a774f21e63c7e5d9256c8bf93f3657509e256059
jricker <james.ricker@gmail.com>
2017-08-10 07:40:37 +0200

    August 9th
    work

--- Assets/scripts/Inventory/UIEventHandler.cs
+++ Assets/scripts/Inventory/UIEventHandler.cs
@@ -7,6 +7,11 @@ public class UIEventHandler : MonoBehaviour {
     public delegate void ItemEventHandler(Item item);
     public static event ItemEventHandler OnItemAddedToInventory;
 
+    public static void ItemAddedToInventory(Item item)
+    {
+        OnItemAddedToInventory(item);
+    }
+
 	// Use this for initialization
 	void Start () {
 		
